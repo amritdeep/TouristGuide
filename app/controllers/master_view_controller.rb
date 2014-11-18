4063fedd582dce5f5e6d0a10dc058cb85b5da115
amritdeep <amritdeepdhungana@hotmail.com>
2014-11-18 14:49:26 +0545

    make create method to work in data are save and display in show page

--- app/controllers/master_view_controller.rb
+++ app/controllers/master_view_controller.rb
@@ -4,9 +4,21 @@ class MasterViewController < ApplicationController
   end
 
   def new
+  	@master = Master.new
   end
 
   def create
   	@master = Master.create(name: params[:master][:name], description: params[:master][:description])
+
+  	respond_to do |format|
+  		if @master.save
+  			format.html { redirect_to master_show_url(@master)}
+  		else
+  			format.html { render :new }
+  		end
+  	end
+  end
+
+  def show
   end
 end
