70ba4309de1039775e718392b5685e07c5fd5449
amritdeep <amritdeepdhungana@hotmail.com>
2014-11-18 13:49:23 +0545

    define routes for create and new action for master page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'master_view/index'
+  get 'master_view/new'
+  post 'master_view/create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
