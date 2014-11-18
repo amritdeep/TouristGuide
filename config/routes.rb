7b8a2fa9e785eab4ed6b8c76dc931cd9fa289ed7
amritdeep <amritdeepdhungana@hotmail.com>
2014-11-18 14:10:29 +0545

    name the create master routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'master_view/index'
   get 'master_view/new'
-  post 'master_view/create'
+  post 'master_view/create', as: :masters
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
