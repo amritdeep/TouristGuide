c8b64b59d58cfeaa0859a9ec7ce3c8b1149cd964
amritdeep <amritdeepdhungana@hotmail.com>
2014-11-19 17:34:23 +0545

    make routes for master and secondary page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   get 'master_view/index'
 
+  get 'masters/:id/secondaries/new' => 'secondaries#master', as: :master_secondaries
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
