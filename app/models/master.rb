5dd36c6fa0fbb6aa1654657ebfe66634257a8bd7
amritdeep <amritdeepdhungana@hotmail.com>
2014-11-25 12:26:41 +0545

    Remove elasticsearch methology and implemented simple searche methodology

--- app/models/master.rb
+++ app/models/master.rb
@@ -1,15 +1,15 @@
-require 'elasticsearch/model'
+# require 'elasticsearch/model'
 
 class Master < ActiveRecord::Base
 
 	## For Advancd Search
-	include Elasticsearch::Model
-  	include Elasticsearch::Model::Callbacks
+	# include Elasticsearch::Model
+ 	#include Elasticsearch::Model::Callbacks
 
 	validates :name, presence: true
 	validates :description, presence: true
 	has_many :secondaries
 
 	scope :master_search, -> (query) { where("name like ?", "%#{query}%") }
-	
+
 end
